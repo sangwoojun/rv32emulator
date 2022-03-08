a925553295cc6b7e1838e21713638dddaad9c25b
Sang-Woo Jun <swjun@ics.uci.edu>
2022-03-07 23:53:14 -0800

    cleaning code

--- cachesim.h
+++ cachesim.h
@@ -5,14 +5,16 @@
 #define CACHESIM_H__
 
 #define CACHE_SETS_SZ 8
-#define CACHE_SETS (1<<CACHE_SETS_SZ)
 #define CACHE_WAYS_SZ 0
-#define CACHE_WAYS (1<<CACHE_WAYS_SZ)
 #define CACHE_LINE_WORD_SZ 0
-#define CACHE_LINE_WORD (1<<CACHE_LINE_WORD_SZ)
 
 //extern uint32_t g_cache[CACHE_SETS][CACHE_WAYS][CACHE_LINE_WORD];
 //extern uint32_t g_tags[CACHE_SETS][CACHE_WAYS];
+
+/// DO NOT EDIT
+#define CACHE_SETS (1<<CACHE_SETS_SZ)
+#define CACHE_WAYS (1<<CACHE_WAYS_SZ)
+#define CACHE_LINE_WORD (1<<CACHE_LINE_WORD_SZ)
 
 
 int cache_peek(uint32_t addr, int bytes);
